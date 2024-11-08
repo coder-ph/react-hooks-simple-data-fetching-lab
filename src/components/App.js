5127368d7412afaed1e69c6ed9a3a11856dc6755
Phelix <phelixmbani@gmail.com>
2024-11-08 10:26:30 +0300

    fetching data with state

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false); 
+  const [images, setImages] = useState(null);
+
+  useEffect(() => {
+    
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setImages(data.message);
+        setIsLoaded(true); 
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        
+      });
+  }, []);
+
+  return (
+    <div>
+      {!isLoaded ? (
+        <p>Loading...</p> 
+      ) : (
+        <img src={images} alt="A Random Dog" /> 
+      )}
+    </div>
+  );
+}
+
+export default App;
